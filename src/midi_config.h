39d7811d40ee8f0a2f3bd0a2bf11e7d61123ffe8
Christopher Gronwald <chris@gronwald.email>
2021-12-03 22:00:39 +0100

    MIDI config nach PCB-Layout sortiert

--- src/midi_config.h
+++ src/midi_config.h
@@ -5,8 +5,8 @@
 
 // MIDI config
 const byte midiChannel = 0;
-const byte butNotes[] = { 1, 2, 3, 4, 5, 6, 7, 8 };
-const byte arcNotes[] = { 11, 12, 13, 14, 15, 16, 17, 18 };
-const byte potNotes[] = { 21, 22, 23 };
+const byte butNotes[] = { 5, 6, 7, 4, 3, 0, 2, 1 }; // ab C0
+const byte arcNotes[] = { 17, 18, 19, 16, 12, 15, 13, 14 }; // ab C1
+const byte potNotes[] = { 20, 21, 22 };
 
 #endif

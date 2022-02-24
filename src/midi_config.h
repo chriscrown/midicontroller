0790236fd44522056c0156f75f30f69a5bd6041f
Christopher Gronwald <chris@gronwald.email>
2022-02-24 15:39:36 +0100

    MIDI channel set to 1

--- src/midi_config.h
+++ src/midi_config.h
@@ -4,7 +4,7 @@
 #include <Arduino.h>
 
 // MIDI config
-const byte midiChannel = 0;
+const byte midiChannel = 1;
 const byte butNotes[] = { 5, 6, 7, 4, 3, 0, 2, 1 }; // ab C0
 const byte arcNotes[] = { 17, 18, 19, 16, 12, 15, 13, 14 }; // ab C1
 const byte potNotes[] = { 20, 21, 22 };
